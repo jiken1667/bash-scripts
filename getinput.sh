f8ce0a278b697c34ca3ea73142bec1c1e9086739
jiken1667 <55505978+jiken1667@users.noreply.github.com>
2019-09-19 07:04:25 +0800

    read the inputs from user and print the values

---
+++ getinput.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please answer the question below."
+read -p "What is your name? " name
+read -p "How old are you? " age
+
+echo "Your name is $name and you are $age years old."
+
+: '
+Commands details
+
+read - is used to read the input from user and store it to a variable name and age
+echo - is used to display the texts in terminal
+'
