acf57e4f54e74708b05e34b76765398242719a08
jiken1667 <55505978+jiken1667@users.noreply.github.com>
2019-09-19 12:20:15 +0800

    this will print numbers from 1 to 10

---
+++ forloopincrement.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for ((counter=1; counter<=10; counter++))
+do
+ echo -n "$counter "
+done
+printf "\n"
+
+: '
+Details
+
+We set the counter to 1 then perform the incrementation printing inside the for loop statement of the variable counter 
+until it is less than or equal to 10
+'
